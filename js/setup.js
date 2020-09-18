53b863c781339993e32295304ad3e09542fe7075
Gustavo Matheus Cerqueira <66489269+gmcerqueira@users.noreply.github.com>
2020-09-17 22:12:13 -0300

    Add files via upload

---
+++ js/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 2
+    },
+    500: {
+      items: 3
+    },
+    800: {
+      items: 5
+    },
+    1000: {
+      items: 7
+    }
+  }
+})
